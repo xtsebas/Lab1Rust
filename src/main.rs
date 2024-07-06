8a4e3cdc3b45814e1cb2a5a01315fa252498e8c3
xtsebas <thiagohugo2014@gmail.com>
2024-07-05 20:11:17 -0600

    Poligon-4

--- src/main.rs
+++ src/main.rs
@@ -106,6 +106,34 @@ fn main() {
         Vec3::new(436.0, 249.0, 0.0),
     ];
 
+    let vertices4 = vec![
+        Vec3::new(413.0, 177.0, 0.0),
+        Vec3::new(448.0, 159.0, 0.0),
+        Vec3::new(502.0, 88.0, 0.0),
+        Vec3::new(553.0, 53.0, 0.0),
+        Vec3::new(535.0, 36.0, 0.0),
+        Vec3::new(676.0, 37.0, 0.0),
+        Vec3::new(660.0, 52.0, 0.0),
+        Vec3::new(750.0, 145.0, 0.0),
+        Vec3::new(761.0, 179.0, 0.0),
+        Vec3::new(672.0, 192.0, 0.0),
+        Vec3::new(659.0, 214.0, 0.0),
+        Vec3::new(615.0, 214.0, 0.0),
+        Vec3::new(632.0, 230.0, 0.0),
+        Vec3::new(580.0, 230.0, 0.0),
+        Vec3::new(597.0, 215.0, 0.0),
+        Vec3::new(552.0, 214.0, 0.0),
+        Vec3::new(517.0, 144.0, 0.0),
+        Vec3::new(466.0, 180.0, 0.0),
+    ];
+    
+    let vertices5 = vec![
+        Vec3::new(682.0, 175.0, 0.0),
+        Vec3::new(708.0, 120.0, 0.0),
+        Vec3::new(735.0, 148.0, 0.0),
+        Vec3::new(739.0, 170.0, 0.0),
+    ];
+
 
 
     // Define the colors
@@ -117,16 +145,26 @@ fn main() {
     
     let line_color3 = 0xFFFFFF;
     let fill_color3 = 0xFF0000;
+    
+    let line_color4 = 0xFFFFFF;
+    let fill_color4 = 0x00FF00;
+
+    let line_color5 = 0xFFFFFF;
+    let fill_color5 = 0xFFFFFF;
 
     // Draw a polygon using the defined vertices
     draw_polygon(&mut framebuffer, &vertices1, line_color1);
     draw_polygon(&mut framebuffer, &vertices2, line_color2);
     draw_polygon(&mut framebuffer, &vertices3, line_color3);
+    draw_polygon(&mut framebuffer, &vertices4, line_color4);
+    draw_polygon(&mut framebuffer, &vertices5, line_color5);
 
     // Fill the polygon
     fill_polygon(&mut framebuffer, &vertices1, fill_color1);
     fill_polygon(&mut framebuffer, &vertices2, fill_color2);
     fill_polygon(&mut framebuffer, &vertices3, fill_color3);
+    fill_polygon(&mut framebuffer, &vertices4, fill_color4);
+    fill_polygon(&mut framebuffer, &vertices5, fill_color5);
 
     // Save the framebuffer as a BMP file
     framebuffer.render_buffer("out.bmp");
