89a3c430b179037a51cc20c1624a3a35e61c35f0
xtsebas <thiagohugo2014@gmail.com>
2024-07-05 19:53:53 -0600

    feat: poligon-1

--- src/main.rs
+++ src/main.rs
@@ -80,6 +80,18 @@ fn main() {
     framebuffer.clear();
 
     // Define los vertices del poligono
+    let vertices = vec![
+        Vec3::new(165.0, 380.0, 0.0),
+        Vec3::new(185.0, 360.0, 0.0),
+        Vec3::new(180.0, 330.0, 0.0),
+        Vec3::new(207.0, 345.0, 0.0),
+        Vec3::new(233.0, 330.0, 0.0),
+        Vec3::new(230.0, 360.0, 0.0),
+        Vec3::new(250.0, 380.0, 0.0),
+        Vec3::new(220.0, 385.0, 0.0),
+        Vec3::new(205.0, 410.0, 0.0),
+        Vec3::new(193.0, 383.0, 0.0),
+    ];
 
 
     // Define the colors
@@ -93,5 +105,5 @@ fn main() {
     fill_polygon(&mut framebuffer, &vertices, fill_color);
 
     // Save the framebuffer as a BMP file
-    framebuffer.render_buffer("filled_polygon.bmp");
+    framebuffer.render_buffer("out.bmp");
 }
